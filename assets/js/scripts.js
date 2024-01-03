36087aa026ab09e96b9fc4d98a798677b7f5fbe0
Jared Elliott <bootcamp@jaredclt.com>
2024-01-02 19:54:27 -0500

    Added comments to file on how the API works/responds.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,2 +1,42 @@
-// API Key: 9b3845b57607784634ed605cf72e7f06
-// 5-Day Forcast: https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=
+/*  API Key: 9b3845b57607784634ed605cf72e7f06
+    5-Day Forcast: https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=9b3845b57607784634ed605cf72e7f06
+    Ex: https://api.openweathermap.org/data/2.5/forecast?lat=35.1149&lon=-80.705&appid=9b3845b57607784634ed605cf72e7f06
+        {
+        "cod": "200",
+        "message": 0,
+        "cnt": 40,
+        "list": [
+            {
+                "dt": 1704250800,
+                "main": {
+        ----- snipped output -----
+    
+    ZipCode: http://api.openweathermap.org/geo/1.0/zip?zip={zip},{countrycode}&appid=9b3845b57607784634ed605cf72e7f06
+    Ex: http://api.openweathermap.org/geo/1.0/zip?zip=28105,US&appid=9b3845b57607784634ed605cf72e7f06
+        {
+            "zip": "28105",
+            "name": "Mecklenburg County",
+            "lat": 35.1149,
+            "lon": -80.705,
+            "country": "US"
+        }
+
+    City Name: http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+    Ex: http://api.openweathermap.org/geo/1.0/direct?q=Charlotte,NC,US&limit=1&appid=9b3845b57607784634ed605cf72e7f06
+        {
+            "name": "Charlotte",
+            "local_names": {
+                "ar": "شارلت",
+                "ur": "شارلٹ",
+                "ru": "Шарлотт",
+                "uk": "Шарлотт",
+                "en": "Charlotte",
+                "fa": "شارلوت",
+                "mk": "Шарлот"
+            },
+            "lat": 35.2272086,
+            "lon": -80.8430827,
+            "country": "US",
+            "state": "North Carolina"
+        }
+*/
